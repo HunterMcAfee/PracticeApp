e0ff49b8ca4ca7b681064b4bee19f5ea85acd6a2
HunterMcAfee <huntmac10@gmail.com>
2017-10-19 19:34:54 -0400

    Wrote schema models for users, lists, and todos.

---
+++ db/schema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const todoSchema = mongoose.Schema({
+    todoTitle: String,
+    listDescription: String,
+    status: Boolean
+});
+
+const listSchema = mongoose.Schema({
+    listTitle: String,
+    listDescription: String,
+    todos: [todoSchema]
+});
+
+const userSchema = mongoose.Schema({
+    userName: String,
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    lists: [listSchema]
+});
+
+const userModel = mongoose.model('User', userSchema);
+const listModel = mongoose.model('List', listSchema);
+const todoModel = mongoose.model('Todo', todoSchema);
+
+module.exports = {
+    userModel, listModel, todoModel
+}
