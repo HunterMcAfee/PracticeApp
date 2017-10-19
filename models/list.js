c5ac1cc152d1c67ae03924ea4cb6fad8ba9a1f07
HunterMcAfee <huntmac10@gmail.com>
2017-10-19 19:41:07 -0400

    Set up model exports for users, lists, and todos

---
+++ models/list.js
@@ -0,0 +1,5 @@
+const Schema = require('../db/schema');
+const mongoose = require('mongoose');
+
+const List = Schema.listModel;
+module.exports = List;
---
+++ models/todo.js
@@ -0,0 +1,5 @@
+const Schema = require('../db/schema');
+const mongoose = require('mongoose');
+
+const Todo = Schema.todoModel;
+module.exports = Todo;
