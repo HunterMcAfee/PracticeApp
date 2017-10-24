856c79b8599f4ff892c4205083f05c2f0ad23508
HunterMcAfee <huntmac10@gmail.com>
2017-10-24 03:54:57 -0400

    Added update list API

--- controllers/list.js
+++ controllers/list.js
@@ -30,26 +30,30 @@ router.get('/:userId/:listId', (req, res) => {
 router.post('/:userId', (req, res) => {
     let newList = new List(req.body.payload);
     User.findById(req.params.userId)
-    .then( (user) => {
-        user.lists.push(newList);
-        user.save();
-        console.log(`New list was created for User ID ${req.params.userId}`);
-    })
-    .catch( (err) => {
-        console.log(err);
-    });
- 
+        .then( (user) => {
+            user.lists.push(newList);
+            user.save();
+            console.log(`New list was created for User ID ${req.params.userId}`);
+        })
+        .catch( (err) => {
+            console.log(err);
+        });
 });
 
-// router.put('/:userId', (req, res) => {
-//     User.findByIdAndUpdate(req.params.userId, req.body.payload)
-//         .then( (user) => {
-//             console.log(`User with ID ${req.params.userId} was updated.`)
-//         })
-//         .catch( (err) => {
-//             console.log(err);
-//         });
-// });
+router.put('/:userId/:listId', (req, res) => {
+    User.findById(req.params.userId)
+        .then( (user) => {
+            let foundList = user.lists.find((list) => {
+                return list.id === req.params.listId
+            })
+            foundList = req.body.payload;
+            user.save();
+            console.log(`List was updated.`);
+        })
+        .catch( (err) => {
+            console.log(err);
+        });
+});
 
 // router.delete('/:userId', (req, res) => {
 //     User.findByIdAndRemove(req.params.userId)
