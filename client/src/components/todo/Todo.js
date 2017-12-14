e35f04e1b474e6f543cbe8709b92a5443872c943
HunterMcAfee <huntmac10@gmail.com>
2017-12-14 11:00:20 -0500

    Added delete button and navigation back to list

--- client/src/components/todo/Todo.js
+++ client/src/components/todo/Todo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Todo extends Component {
     constructor() {
@@ -26,12 +27,30 @@ class Todo extends Component {
         }
     }
 
+    _deleteTodo = async (e) => {
+        e.preventDefault();
+        try {
+            let userId = this.props.match.params.userId;
+            let listId = this.props.match.params.listId;
+            let todoId = this.props.match.params.todoId;
+            const res = await axios.delete(`/api/todo/${userId}/${listId}/${todoId}`);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
     render() {
+        let userId = this.props.match.params.userId;
+        let listId = this.props.match.params.listId;
         return (
             <div>
+                <button onClick={this._deleteTodo}>Delete</button>
+                <br />
                 <div>Title: {this.state.todo.todoTitle}</div>
                 <div>Description: {this.state.todo.todoDescription}</div>
                 <div>Status: {this.state.todo.status ? "Complete" : "Incomplete"}</div>
+                <Link to={`/user/${userId}/list/${listId}`}><button>Go back</button></Link>
             </div>
         );
     }
